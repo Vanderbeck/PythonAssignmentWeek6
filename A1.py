f8aa7dc8fe913fd1e60ee7126c4551618e449f33
R Lindsay Vanderbeck <9rlv@queensu.ca>
2016-11-05 15:47:39 -0600

    First iteration. I can access nester JSON info.

--- A1.py
+++ A1.py
@@ -1,1 +1,12 @@
 #Assignment 1
+
+import urllib
+import json
+
+url = 'http://python-data.dr-chuck.net/comments_42.json'
+
+link = urllib.urlopen(url)
+js = json.load(link)
+
+print json.dumps(js, indent=4)
+print js['comments'][2]['count']
